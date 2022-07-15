a73e9c6ad53786bd91bb7d06019f56662a62d4a1
mahdizadehamir <mehdizadeh.amir11@gmail.com>
2022-07-15 17:58:20 +0430

    material ui added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route element={<Home />} path="/" />
+        <Route element={<About />} path="/about" />
+        <Route element={<Contact />} path="/contact" />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
